ae53aaa0c13a37ced4558e34196211c1c296ff39
aReuuuuu <493414881@qq.com>
2020-07-08 12:24:59 +0800

    home

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,23 @@
 <template>
   <div id="app">
     <!-- $route  路由的信息
-    $router 你创建的全局路由对象 -->
-    <!-- name自动生成css过渡类名   Vue系列 -->
+    $router 你创建的全局路由对象
+    name自动生成css过渡类名   Vue系列
     <transition name="router-fade" mode="out-in">
-      <!-- Vue系列 -->
+      Vue系列
       <keep-alive>
-        <!-- VueRouter    渲染路径匹配到的视图组件-->
+        VueRouter    渲染路径匹配到的视图组件
         <router-view v-if="$route.meta.keepAlive"></router-view>
       </keep-alive>
     </transition>
-
     <transition name="router-fade" mode="out-in">
       <router-view v-if="!$route.meta.keepAlive"></router-view>
-    </transition>
-    
+    </transition> -->
+  
+    <keep-alive>
+        <router-view></router-view>
+    </keep-alive>
+
     <svg-icon></svg-icon>
   </div>
 </template>
