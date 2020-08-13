179848832f1a9b124f7162677c45f32286004b26
aReuuuuu <493414881@qq.com>
2020-08-13 20:26:36 +0800

    .

--- src/page/shop/shop.vue
+++ src/page/shop/shop.vue
@@ -22,12 +22,30 @@
                         </span>
                         <span>无门槛</span>
                     </p>
-                    <p>领取</p>
+                    <p class="get-coupon">领取</p>
                 </div>
-                <div></div>
+                <div class="right-coupon">
+                    <p class="coupon-money">
+                        <span>
+                            <small>$</small>10
+                        </span>
+                        <span>满99可用</span>
+                    </p>
+                    <p class="get-coupon">领取</p>
+                </div>
             </div>
             <!-- 优惠券详情 -->
-            <div></div>
+            <div class="shop-discount-detail">
+                <div class="discount-tag">
+                    <div class="tag"><span>满20件撒飒飒</span></div>
+                    <div class="tag"><span>满20件sss</span></div>
+                    <div class="tag"><span>满20件sss</span></div>
+                    <div class="tag"><span>配送优惠</span></div>
+                </div>
+                <span class="discount-show-tag">
+                    n个优惠 <i></i>
+                </span>
+            </div>
         </section>
         <!-- 切换商品模块栏 -->
         <section></section>
@@ -36,6 +54,11 @@
 
 <script>
 export default {
+    data(){
+        return{
+
+        }
+    },
 }
 </script>
 
@@ -84,21 +107,107 @@ export default {
     }
     //中间优惠券
     .shop-coupon{
-            background-color: red;
             display: flex;
+            margin-top: .4rem;
             flex-direction: row;
-            justify-content: space-around;
+            justify-content: center;
+            font-size: .4rem;
             .left-coupon{
+                background-color: #ffe578;
                 @include wh(38.4vw,6.4vw);
                 display: flex;
                 flex-direction: row;
-                margin-left: 1rem;
+                align-items: center;
                 .coupon-money{
+                    align-items: center;
                     display: flex;
                     flex-direction:row;
+                    span:nth-of-type(2){
+                        margin-left: .4rem;
+                    }
+                }
+                //领取
+                .get-coupon{
+                    margin-left: .4rem;
+                }
+            }
+
+            .right-coupon{
+                margin-left: .6rem;
+                background-color: #fff4f4;
+                @include wh(38.4vw,6.4vw);
+                display: flex;
+                flex-direction: row;
+                align-items: center;
+                .coupon-money{
+                    align-items: center;
+                    display: flex;
+                    flex-direction:row;
+                    span:nth-of-type(2){
+                        margin-left: .4rem;
+                    }
+                }
+                //领取
+                .get-coupon{
+                    margin-left: .4rem;
+                }
+            }    
+        
+    }
+
+    .shop-discount-detail{
+        margin: .3rem auto 0;
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        // align-content: center;
+        align-items: center;
+        position: relative;
+        @include wh(80vw,2.5rem);
+        .discount-tag{
+            display: flex;
+            // justify-content: space-between;
+            // flex: 1;
+            overflow: hidden;            
+            .tag{
+                display: inline-block;
+                // transform-origin: top left;
+                // -webkit-transform: scale(0.8);
+                font-size: .5rem;
+                margin-left: .3rem;
+                color: red;
+                border: .1rem solid red;
+                white-space: nowrap;
+                overflow: hidden;
+                text-overflow: ellipsis;
+                span{
+                    padding: 0 .1rem;
                 }
             }
         }
+        .discount-show-tag{
+            margin-left: 1rem;
+            color: #999;
+            font-size: .5rem;
+            // overflow: hidden;
+            i{
+                position: relative;
+            }
+            i::after{
+                position: absolute;
+                top: 50%;
+                transform: translateY(-50%);
+                margin-left: .3rem;
+                content: "";
+                border-style: solid;
+                border-width: .4rem .4rem 0 .4rem;
+                border-color: rgba(0, 0, 0, .3) transparent transparent transparent;
+            }
+        }
+        
+    }
+
+
 }
 
 </style>
